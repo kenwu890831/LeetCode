b87c1d57720580972f9426c1cc87eddc93964799
kenwu890831 <92260086+kenwu890831@users.noreply.github.com>
2025-01-28 17:36:56 +0800

    Add files via upload

--- LeetCode_ 1462. Course Schedule IVMeeting.cpp
+++ LeetCode_ 1462. Course Schedule IVMeeting.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<bitset<100>> prereq( n ) ;
-        vector<vector<int>> adj( n ) ;
-        vector<int> inDegree( n, 0 ) ;
+        vector< bitset< 100 > > temp( n ) ;
+        vector< int > degree( n, 0 ) ; // 
+        vector< vector< int > > adj( n ) ; 
+        vector< bool > result ;
+        queue < int > q ;
 
-        for ( auto& edge : prerequisites ) {
-            adj[edge[0]].push_back(edge[1]) ;
-            prereq[edge[1]].set( edge[0] ) ;
-            inDegree[edge[1]]++ ;
+        for ( auto& pre : prerequisites ) {
+            
+            adj[ pre[0] ].push_back( pre[1] ) ;
+            temp[pre[1]].set( pre[0] ) ;
+            degree[ pre[1] ]++ ;
         }
 
-        queue< int > q ;
-        for ( int i = 0; i < n; ++i )
-            if (inDegree[i] == 0) 
-              q.push(i) ;
+        for( int i = 0 ; i < degree.size(); i++ ) {
+            if ( degree[i] == 0 ) 
+              q.push( i ) ;
+        }
 
-        while ( !q.empty() ) {
-            int u = q.front() ;
+        while( !q.empty() ) {
+            int cur = q.front() ;
             q.pop() ;
-            for ( int v : adj[u] ) {
-                prereq[v] |= prereq[u] ;
-                if ( --inDegree[v] == 0  )
-                  q.push(v) ;
+
+            for( int& child : adj[ cur] ) {
+                temp[ child ] |= temp[ cur ] ;
+                if ( --degree[ child] == 0 )
+                  q.push( child ) ;
             }
         }
 
-        vector< bool > ans ;
-        for ( auto& qry : queries )
-            ans.push_back( prereq[qry[1]][qry[0]] ); 
+        for ( auto& a : queries )
+            result.push_back( temp[a[1]][a[0]] ); 
 
-        return ans ;
+
+
+        return result ;
+
+
+        
     }
 };
