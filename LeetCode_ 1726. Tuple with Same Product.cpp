bb67432483d6162c22fe3d7edbfa34b8baeb012d
kenwu890831 <92260086+kenwu890831@users.noreply.github.com>
2025-02-06 16:31:28 +0800

    Add files via upload

--- LeetCode_ 1726. Tuple with Same Product.cpp
+++ LeetCode_ 1726. Tuple with Same Product.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
-        unordered_map < int, int > hash ;
-        int result = 0, n = nums.size() ;
+        vector< int > temp ;
+        int result = 0, n = nums.size(), walk = 0, count = 0 ;
         for ( int i = 0 ; i < n - 1 ; i++ ) {
             for ( int j = i + 1 ; j < n ; j++ ) {
-                int mul = nums[i]* nums[j] ;
-                result += 8*hash[mul] ;
-                hash[mul]++ ;
+                temp.push_back( nums[i]* nums[j] ) ;
             }
         }
+
+        sort( temp.begin(), temp.end() ) ;
+
+        for ( int i = 1 ; i < temp.size() ; i++ ) {
+            if ( temp[i] == temp[walk] )
+              count++ ;
+            else {
+                if ( count > 1 )
+                  result += 4*count*(count-1) ;
+                walk = i ;
+                count = 1 ;
+            }
+        }
+
 
 
 
