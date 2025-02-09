97f939dae34c1859f1e5922efd6157fa78fd8231
kenwu890831 <92260086+kenwu890831@users.noreply.github.com>
2025-02-09 21:45:23 +0800

    Add files via upload

---
+++ LeetCode_ 2364. Count Number of Bad Pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        long long good = 0 ;
+        long long n = nums.size() ;
+        unordered_map< int, int > temp ;
+
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            int k = nums[i] - i ;
+            good += temp[k] ;//check same
+            temp[k]++ ;
+        }
+
+
+
+        return  n * ( n - 1 ) / 2  - good ;
+    }
+};
