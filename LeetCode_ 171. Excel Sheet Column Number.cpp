5b267aa14aae1acbc7f8a78c2ff8ef451d7e2039
kenwu890831 <92260086+kenwu890831@users.noreply.github.com>
2026-08-10 21:32:40 +0800

    Add files via upload

---
+++ LeetCode_ 171. Excel Sheet Column Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int ans = 0 ;
+        long long mul = 1 ;
+
+        for ( int i = columnTitle.size() - 1 ; i >= 0 ; i-- ) {
+            
+            ans += ( columnTitle[i] - 'A' + 1) * mul ;
+            mul *= 26 ;
+        }
+
+
+        return ans ;
+    }
+};
