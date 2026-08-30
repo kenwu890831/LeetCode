1bd862b53f526daf0606afc64778f2f4251559df
kenwu890831 <92260086+kenwu890831@users.noreply.github.com>
2026-08-30 13:06:41 +0800

    Add files via upload

---
+++ LeetCode_ 205. Isomorphic Strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map< char, char > m1 , m2 ;
+        for( int i = 0 ; i < s.size() ; i++ ) {
+            if ( m1[s[i]] == NULL && m2[t[i]] == NULL ) {
+                m1[s[i]] = t[i] ;
+                m2[t[i]] = s[i] ;
+            }
+            
+            if ( m1[s[i]] != t[i] && m2[t[i]] != s[i] )
+                return false ;
+        }
+
+        return true ;
+    }
+};
